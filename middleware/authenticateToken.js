7206de931139ecfb5fd2ca2fc5e8b1d0f6853fb9
Navneet <navneetyadav9198977444@gmail.com>
2024-02-25 21:58:27 +0530

    news changes

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -3,8 +3,17 @@ const jwt = require("jsonwebtoken");
 const secretKey = "hello6g7yg678g8y"; // Replace with a strong secret in production
 
 const authenticateToken = (req, res, next) => {
-  console.log(req.header("Authorization"));
-  const token = req.header("Authorization").split("")[1];
+  const authHeader = req.header("Authorization");
+  if (!authHeader) {
+    return res.status(401).json({
+      error: "Unauthorized",
+      message: "Missing token in the request header",
+    });
+  }
+
+  const token = authHeader.split(" ")[1]; // Split by space and get the second element
+  console.log(token);
+
   if (!token) {
     return res.status(401).json({
       error: "Unauthorized",
